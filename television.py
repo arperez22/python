08cecc61995c85f2645fe37150f2ac0baab0ab06
arperez22 <aperez21@huskers.unl.edu>
2024-11-22 21:48:20 -0600

    Added test_television.py and changes to television.py

--- television.py
+++ television.py
@@ -6,26 +6,26 @@ class Television:
     CURRENT_VOLUME: int = 0
 
     def __init__(self) -> None:
-        '''
+        """
         Method to set default values of a television object
-        '''
+        """
         self.__status: bool = False
         self.__muted: bool = False
         self.__volume: int = self.MIN_VOLUME
         self.__channel: int = self.MIN_CHANNEL
 
     def power(self) -> None:
-        '''
+        """
         Method to toggle the power status of a television Object
-        '''
+        """
         self.__status ^= True
 
     def mute(self) -> None:
-        '''
+        """
         Method to toggle the mute status of a television object.
         Saves the volume before television is muted to be displayed
         after television is unmuted.
-        '''
+        """
         if not self.__status:
             return
 
@@ -33,12 +33,12 @@ class Television:
         self.CURRENT_VOLUME, self.__volume = self.__volume, self.CURRENT_VOLUME
 
     def channel_up(self) -> None:
-        '''
+        """
         Method to increase the channel of a television object.  If the channel
         of the television object is equal to the MAX_CHANNEL class variable after
         increasing the channel, the channel number will wrap around to the MIN_CHANNEL\
         class variable.
-        '''
+        """
         if not self.__status:
             return
 
@@ -48,12 +48,12 @@ class Television:
             self.__channel += 1
 
     def channel_down(self) -> None:
-        '''
+        """
         Method to decrease the channel of a television object.  If the channel
         of the television object is equal to the MIN_CHANNEL class variable after
         decreasing the channel, the channel number will wrap around to the MAX_CHANNEL
         class variable.
-        '''
+        """
         if not self.__status:
             return
 
@@ -63,11 +63,11 @@ class Television:
             self.__channel -= 1
 
     def volume_up(self) -> None:
-        '''
+        """
         Method to increase the volume of a television object.  If the volume
         of the television object is equal to the MAX_VOLUME class variable before
         increasing the volume, the volume will not be increased.
-        '''
+        """
         if not self.__status:
             return
 
@@ -80,11 +80,11 @@ class Television:
         self.__volume += 1
 
     def volume_down(self) -> None:
-        '''
+        """
          Method to decrease the volume of a television object.  If the volume
          of the television object is equal to the MIN_VOLUME class variable before
          decreasing the volume, the volume will not be decreased.
-        '''
+        """
         if not self.__status:
             return
 
@@ -97,8 +97,8 @@ class Television:
         self.__volume -= 1
 
     def __str__(self) -> str:
-        '''
+        """
         Method to access a string representation of a television object
         :return: String representation of the television object
-        '''
+        """
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
---
+++ test_television.py
@@ -0,0 +1,111 @@
+import pytest
+from television import *
+
+class Test:
+
+    def setup_method(self):
+        self.tv1 = Television()
+        self.tv2 = Television()
+
+    def teardown_method(self):
+        del self.tv1
+        del self.tv2
+
+    def test_init(self):
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_power(self):
+        # Power Television On
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        # Power Television Off
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_mute(self):
+        # Power Television On
+        self.tv1.power()
+        # Increase Volume Once
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
+        # Mute Television
+        self.tv1.mute()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        # Unmute Television
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
+        # Power Television Off and Mute
+        self.tv1.power()
+        self.tv1.mute()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 1'
+        # Power Television On and Unmute
+        self.tv1.power()
+        self.tv1.mute()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
+
+
+
+    def test_channel_up(self):
+        # Increase Channel while Television is Off
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+        # Power Television On and Increase Channel
+        self.tv1.power()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 1, Volume = 0'
+        # Increase Channel Past Maximum Value
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+
+    def test_channel_down(self):
+        # Decrease Channel While Television is Off
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+        # Power Television On and Decrease Channel Past Minimum Channel
+        self.tv1.power()
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 3, Volume = 0'
+
+
+    def test_volume_up(self):
+        # Increase Volume While Television is Off
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+        # Power Television On and Increase Volume
+        self.tv1.power()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
+        # Mute Television and Increase Volume
+        self.tv1.mute()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 2'
+        # Increase Volume Past Maximum Volume
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 2'
+
+
+
+
+    def test_volume_down(self):
+        # Decrease Volume While Television is Off
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+        # Power Television On, Increase Volume to the Maximum, and Decrease the Volume
+        self.tv1.power()
+        self.tv1.volume_up()
+        self.tv1.volume_up()
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 1'
+        # Mute Television and Decrease Volume
+        self.tv1.mute()
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        # Decrease Volume Past the Minimum Value
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+
+
+
